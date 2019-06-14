7b34930c3d6afcef8ea16b3c21307a6022e3a509
LoneWolf96 <irash.96@gmail.com>
2019-06-14 02:57:55 -0500

    third commit

--- pokercalculator/pokercalculator/app.py
+++ pokercalculator/pokercalculator/app.py
@@ -1,30 +1,33 @@
 import os
+from final_class import final
 from .calculator import Calculator
 
 
+@final
 class App:
 
     @staticmethod
     def main():
         try:
-            nextLine = input(
+            next_line = input(
                 "Enter the pokerdata.txt source directory" +
                 "(Press Enter for default):\n")
-            sourcePath = App.__get_folder(nextLine, "pokerdata.txt")
+            source_path = App.__get_folder(next_line, "pokerdata.txt")
 
-            nextLine = input(
+            next_line = input(
                 "Enter the poker_results.txt target directory" +
                 "(Press Enter for default):\n")
-            targetPath = App.__get_folder(nextLine, "poker_results.txt")
+            target_path = App.__get_folder(next_line, "poker_results.txt")
 
-            if (Calculator(sourcePath).print_results(targetPath)):
+            if (Calculator(source_path).print_results(target_path)):
                 print("Successful results in your folder.\n")
             else:
                 print("There's been an error processing the information.\n")
-
         except Exception as e:
             print(str(e))
 
     @staticmethod
-    def __get_folder(line, fileName):
-        return (line + "\\" if len(line) > 0 else os.getcwd() + "/") + fileName
+    def __get_folder(line, file_name):
+        return (line + "\\"
+                if len(line) > 0
+                else os.getcwd() + "/") + file_name
--- pokercalculator/pokercalculator/calculator.py
+++ pokercalculator/pokercalculator/calculator.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from final_class import final
 
 
@@ -7,7 +8,7 @@ class Calculator:
     def __init__(self, filePath):
         self.__winners = [0, 0, 0]
         self.__games = 0
-        self.calculate(filePath)
+        self.__calculate(filePath)
 
     @property
     def winners(self):
@@ -17,12 +18,94 @@ class Calculator:
     def games(self):
         return self.__games
 
-    def calculate(self, filePath):
-        print(f"{filePath}\n")
+    def print_results(self, file_path):
+        try:
+            file = open(file_path, "w")
 
-    def print_results(self, filePath):
-        print(f"{filePath}\n")
-        return True
+            winners = self.winners
+            games = self.games
+
+            lines = [f"Total Games: {games}\n"]
+
+            for i in range(3):
+                s = f"{(i + 1)}: {winners[i]}\n"
+                lines.append(s)
+
+            p1_percentage = "%.2f" % ((winners[0] / games) * 100)
+            p2_percentage = "%.2f" % ((winners[1] / games) * 100)
+            lines.append(["4:\n",
+                          "--------- PLAYER 1 --------- |" +
+                          " --------- PLAYER 2 ---------\n",
+                          f"           {p1_percentage}%            |" +
+                          f"             {p2_percentage}%          \n",
+                          "---------------------------- |" +
+                          " ----------------------------"])
+
+            now = datetime.now().strftime("%d-%m-%Y, %H:%M:%S")
+            lines.append(now)
+
+            file.writelines(lines)
+            return True
+        except (FileNotFoundError, TypeError) as e:
+            print(str(e))
+            return False
+        finally:
+            file.close()
+
+    def __calculate(self, file_path):
+        poker_data = self.__get_poker_data(file_path)
+
+        if (poker_data is None):
+            return
+
+        poker_array = poker_data.split("-")
+        self.__games = len(poker_array)
+
+        for game in poker_array:
+            game_array = game.replace("\n", "").split(" ")
+            n = len(game_array) + 1
+
+            handString1 = game_array[:n // 2]
+            handString2 = game_array[n // 2:]
+
+            hand1 = 1  # Hand(Hand.from_string(handString1))
+            hand2 = 2  # Hand(Hand.from_string(handString2))
+
+            print(str(handString1) + " - " + str(handString2))
+
+            self.__winners[self.__check_winners(hand1, hand2)] += 1
+
+    def __get_poker_data(self, file_path):
+        try:
+            file = open(file_path, "r")
+            return '-'.join(file)
+        except (FileNotFoundError, TypeError) as e:
+            print(str(e))
+            return None
+        finally:
+            file.close()
+
+    def __break_tie(self, hand1, hand2):
+        pass
+
+    def __break_tie_rest_helper(self, ranks):
+        pass
+
+    def __break_tie_rest(self):
+        pass
+
+    def __break_tie_straight(self):
+        pass
+
+    def __break_tie_high_card(self):
+        pass
+
+    def __check_winners(self, hand1, hand2):
+        hand1Rank = 1  # hand1.get_hand_rank().ordinal()
+        hand2Rank = 2  # hand2.get_hand_rank().ordinal()
+
+        winners = self.__check_winners_helper(hand1Rank, hand2Rank)
+        return winners if winners != 2 else self.__break_tie(hand1, hand2)
 
     def __check_winners_helper(self, num1, num2):
         comparer = (num1 > num2) - (num1 < num2)
@@ -32,7 +115,7 @@ class Calculator:
         winners = self.winners
         games = self.games
 
-        return f"""Total Games: {games}\n
-            Player 1: {winners[0]}\n
-            Player 2: {winners[1]}\n
-            Tie: {winners[2]}"""
+        return (f"Total Games: {games}\n" +
+                f"Player 1: {winners[0]}\n" +
+                f"Player 2: {winners[1]}\n"
+                f"Tie: {winners[2]}")
